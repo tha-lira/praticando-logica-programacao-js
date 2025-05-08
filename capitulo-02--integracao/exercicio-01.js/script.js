95e6468d7060c3c812e8db89a677b43dc9eb54c2
thais <thais.lira.apolinario@gmail.com>
2025-05-08 01:53:55 -0300

    estrutura do exercicio

--- capitulo-02--integracao/exercicio-01.js/script.js
+++ capitulo-02--integracao/exercicio-01.js/script.js
@@ -1,23 +1,45 @@
-const entrada = document.querySelector('nome');
-
-const btnEnviar = document.querySelector('btn-enviar');
-const btnLimpar = document.querySelector('btn-limpar');
+//dom
+const entrada = document.querySelector('#nome');
+const mensagem = document.querySelector('.mensagem-envio'); 
+const btnEnviar = document.querySelector('#btn-enviar');
+const btnLimpar = document.querySelector('#btn-limpar');
+const saidaformulario = document.querySelector('#resposta-formulario');
 
 // Entrada de dados
-const entradaDados = () => entrada.value
+const entradaDados = () => entrada.value.trim()
 
 // Função
-const enviarMensagem = () => {
+const  verificarDados = (event) => {
+ event.preventDefault();
 
+ const entradaUsuario = entradaDados()
+
+ if(!entradaUsuario){
+  mensagem.innerText = 'Insira um nome válido'
+  entrada.style.border = "3px solid red";
+  saidaformulario.style.display = 'block'
+
+ } else{
+  processarEnvio()
+  entrada.style.border = "3px solid green";  
+  saidaformulario.style.display = 'block'
+ }
 }
 
-// Função 
+// Função para processar os dados inseridos e exibir a resposta
+const processarEnvio = () => {
+ const entradaUsuario = entradaDados();
+ mensagem.innerText = `Olá ${entradaUsuario}, Bem-vindo(a)!`; // Exibe uma mensagem de boas-vindas
+}
 
-
-// Função para limpar campos do Form
+// Função para limpar os campos do formulário
 const limparCampos = () => {
+ saidaformulario.style.display = 'none';
+ entrada.style.border = "1px solid #ccc"; 
+ entrada.value = ""; 
+ mensagem.innerText = ''; 
 }
 
-// Evento de 'click' dos botões
-btnEnviar.addEventListener('click', enviarMensagem);
+// Eventos para os botões
+btnEnviar.addEventListener('click', verificarDados);
 btnLimpar.addEventListener('click', limparCampos);
